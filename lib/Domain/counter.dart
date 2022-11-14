e528e570173ff093c066bbd98de1c1e0b726f6fc
Luís Arteiro <lucho14garfield@gmail.com>
2022-11-14 17:53:20 +0000

    feat: Updated counter project. #5

--- lib/Domain/counter.dart
+++ lib/Domain/counter.dart
@@ -1,17 +1,13 @@
 import 'package:equatable/equatable.dart';
 
 class Counter extends Equatable {
-  int value = 0;
+  int value;
 
+  Counter({this.value = 0});
 
   increment() => value++;
-
-
   decrement() => value--;
 
-
-
   @override
-  // TODO: implement props
-  List<Object> get props => [value];
+  List<Object?> get props => [value];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
+import 'package:counter_app/presentation/counter_page.dart';
 import 'package:flutter/material.dart';
-import 'package:counter_app_tdd/Presentation/counter_page.dart';
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+// coverage:ignore-start
+void main() {
+  runApp(const MyApp());
 }
+// coverage:ignore-end
 
 class MyApp extends StatelessWidget {
-  final int number = 1 ;
-   @override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Counter App Example',
-    theme: ThemeData(
-      primaryColor: Colors.red.shade800,
-      accentColor: Colors.red.shade600,
-    ),
-    debugShowCheckedModeBanner: false,
-    home: CounterPage(),
-  );
-}
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CounterPage(title: 'Counter App Demo'),
+    );
+  }
 }
