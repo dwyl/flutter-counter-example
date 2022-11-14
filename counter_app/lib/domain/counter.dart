862c3da954c003146f9b536a5d5eb3393d10a3a4
Luís Arteiro <lucho14garfield@gmail.com>
2022-11-14 17:08:34 +0000

    feat: Refactoring app with proper structure.

---
+++ counter_app/lib/domain/counter.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+
+class Counter extends Equatable {
+  int value;
+
+  Counter({this.value = 0});
+
+  increment() => value++;
+  decrement() => value--;
+
+  @override
+  List<Object?> get props => [value];
+}
---
+++ counter_app/lib/presentation/counter_page.dart
@@ -0,0 +1,66 @@
+import 'package:counter_app/domain/counter.dart';
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  Counter _counter = Counter();
+
+  void _incrementCounter() {
+    setState(() {
+      _counter.increment();
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter.decrement();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              _counter.value.toString(),
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FloatingActionButton(
+                  onPressed: _decrementCounter,
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
